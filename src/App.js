715e96ada2785a22bcdbfb48412027ce34f1ff1b
Igor Quiterio <idcq101@gmail.com>
2020-06-18 21:42:44 -0300

    chore: funcoes de req p api e layout dinamico

--- src/App.js
+++ src/App.js
@@ -1,26 +1,54 @@
-import React from "react";
+import React, { useState, useEffect }  from "react";
+import api from './services/api';
 
 import "./styles.css";
 
 function App() {
+
+  const [repositories, setRepositories] = useState([]);
+
   async function handleAddRepository() {
-    // TODO
+    const response = await api.post('repositories', {
+      "title": `repositorio ${Date.now()}`, 
+      "url": "igor.quiter.io",
+      "techs":"['oi', 'tchau']"
+    })
+    const newRepository = response.data;
+
+    setRepositories([...repositories, newRepository]);
   }
 
   async function handleRemoveRepository(id) {
-    // TODO
+    const response = await api.delete(`repositories/${id}`)
+
+    if(response.status === 204){
+     const repositoryIndex = repositories.findIndex(repository => repository.id === id);
+      repositories.splice(repositoryIndex,1);
+      
+      setRepositories([...repositories]);
+    }
   }
+
+  useEffect(()=>{
+    api.get('repositories').then(response => {
+      setRepositories(response.data);
+    })
+  },[]);
 
   return (
     <div>
       <ul data-testid="repository-list">
-        <li>
-          Repositório 1
+        {repositories.map(repository => {
+          return(
+            <li key={repository.id}>
+              {repository.title}
 
-          <button onClick={() => handleRemoveRepository(1)}>
-            Remover
-          </button>
-        </li>
+              <button onClick={() => handleRemoveRepository(repository.id)}>
+                Remover
+              </button>
+            </li>
+          )
+        })}
       </ul>
 
       <button onClick={handleAddRepository}>Adicionar</button>
